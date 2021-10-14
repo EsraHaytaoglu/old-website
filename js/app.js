e0b12a71a3071a3312735b8b3177984edc1b9a9d
Esra Cebi <esracebi.81@gmail.com>
2021-10-14 19:02:04 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+    $('#loading').hide();
+}) 
+
+const blobCursor = (() => {  
+  const CURSOR = document.querySelector('#cursorBlob');
+  const LINKS = document.querySelectorAll('.nav__link');
+  const setCursorPos = (e) => {
+    const { pageX: posX, pageY: posY } = e;
+    CURSOR.style.top = `${posY - (CURSOR.offsetHeight / 2)}px`;
+    CURSOR.style.left = `${posX - (CURSOR.offsetWidth / 2)}px`;
+  };
+  document.addEventListener('mousemove', setCursorPos);
+  
+  const setCursorHover = () => CURSOR.style.transform = 'scale(2.5)';
+  const removeCursorHover = () => CURSOR.style.transform = '';
+  LINKS.forEach(link => link.addEventListener('mouseover', setCursorHover));
+  LINKS.forEach(link => link.addEventListener('mouseleave', removeCursorHover));
+  
+})();  
+$(function() {
+  var text = $(".text");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+      text.removeClass("hidden");
+    } else {
+      text.addClass("hidden");
+    }
+  });
+});
+
